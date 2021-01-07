c5f684e8484745de4b95b8cbdfcb5382703924f0
Rafael Goncalves <rafaelgoncalves@riseup.net>
2021-01-07 08:35:19 -0300

    Corrige label de botão de upload de imagem

--- src/custom-upload.js
+++ src/custom-upload.js
@@ -1,7 +1,7 @@
-import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
-import Command from '@ckeditor/ckeditor5-core/src/command';
-import ButtonView from '@ckeditor/ckeditor5-ui/src/button/buttonview';
-import imageIcon from '@ckeditor/ckeditor5-core/theme/icons/image.svg';
+import Plugin from "@ckeditor/ckeditor5-core/src/plugin";
+import Command from "@ckeditor/ckeditor5-core/src/command";
+import ButtonView from "@ckeditor/ckeditor5-ui/src/button/buttonview";
+import imageIcon from "@ckeditor/ckeditor5-core/theme/icons/image.svg";
 // import CustomUploadApdapter from './upload-adapter';
 // import View from '@ckeditor/ckeditor5-ui/src/view';
 
@@ -16,212 +16,118 @@ import imageIcon from '@ckeditor/ckeditor5-core/theme/icons/image.svg';
 // }
 
 export class BatataUI extends Plugin {
-	pluginName() {
-		return 'BatataUI';
-	}
+  pluginName() {
+    return "BatataUI";
+  }
 
-	init() {
-		const editor = this
-			.editor;
-		const {
-			t,
-			ui: {
-				componentFactory
-			}
-		} = editor;
+  init() {
+    const editor = this.editor;
+    const {
+      t,
+      ui: { componentFactory },
+    } = editor;
 
-		componentFactory.add(
-			'insertImage',
-			locale => {
-				const command = editor.commands.get(
-					'batata'
-				);
+    componentFactory.add("insertImage", (locale) => {
+      const command = editor.commands.get("batata");
 
-				const button = new ButtonView(
-					locale
-				);
+      const button = new ButtonView(locale);
 
-				button.set(
-					{
-						label: t(
-							'Insira uma uma imagem'
-						),
-						icon: imageIcon,
-						tooltip: true
-					}
-				);
+      button.set({
+        label: t("Insira uma imagem"),
+        icon: imageIcon,
+        tooltip: true,
+      });
 
-				button.bind(
-					'isEnabled'
-				).to(
-					command
-				);
+      button.bind("isEnabled").to(command);
 
-				button.on(
-					'execute',
-					() => {
-						editor.execute(
-							'batata'
-						);
-						editor.editing.view.focus();
-					}
-				);
+      button.on("execute", () => {
+        editor.execute("batata");
+        editor.editing.view.focus();
+      });
 
-				return button;
-			}
-		);
-	}
+      return button;
+    });
+  }
 }
 
 export class BatataCommand extends Command {
-	constructor(
-		editor
-	) {
-		super(
-			editor
-		);
-		this.stopListening(
-			this
-				.editor
-				.model
-				.document,
-			'change'
-		);
-		this.listenTo(
-			this
-				.editor
-				.model
-				.document,
-			'change',
-			() =>
-				this.refresh(),
-			{
-				priority:
-					'low'
-			}
-		);
+  constructor(editor) {
+    super(editor);
+    this.stopListening(this.editor.model.document, "change");
+    this.listenTo(this.editor.model.document, "change", () => this.refresh(), {
+      priority: "low",
+    });
 
-		const input = document.createElement(
-			'input'
-		);
-		input.id =
-			'ClickCKImageUploadInput';
-		input.type =
-			'file';
-		input.style.display =
-			'none';
-		input.accept =
-			'image/*';
-		document.body.appendChild(
-			input
-		);
+    const input = document.createElement("input");
+    input.id = "ClickCKImageUploadInput";
+    input.type = "file";
+    input.style.display = "none";
+    input.accept = "image/*";
+    document.body.appendChild(input);
 
-		this.input = input;
+    this.input = input;
 
-		this.fileListener = this.fileListener.bind(
-			this
-		);
+    this.fileListener = this.fileListener.bind(this);
 
-		input.addEventListener(
-			'change',
-			this
-				.fileListener
-		);
-	}
+    input.addEventListener("change", this.fileListener);
+  }
 
-	fileListener(
-		evt
-	) {
-		evt.preventDefault();
-		const [
-			image
-		] = evt.target.files;
-		const editor = this
-			.editor;
+  fileListener(evt) {
+    evt.preventDefault();
+    const [image] = evt.target.files;
+    const editor = this.editor;
 
-		editor.config._config
-			.customUpload(
-				image
-			)
-			.then(
-				imageUrl => {
-					editor.model.change(
-						writer => {
-							const imageElement = writer.createElement(
-								'image',
-								{
-									src: imageUrl
-								}
-							);
+    editor.config._config
+      .customUpload(image)
+      .then((imageUrl) => {
+        editor.model.change((writer) => {
+          const imageElement = writer.createElement("image", {
+            src: imageUrl,
+          });
 
-							// Insert the image in the current selection location.
-							editor.model.insertContent(
-								imageElement,
-								editor
-									.model
-									.document
-									.selection
-							);
-						}
-					);
-				}
-			)
-			.catch(
-				err =>
-					console.error(
-						err
-					)
-			);
-	}
+          // Insert the image in the current selection location.
+          editor.model.insertContent(
+            imageElement,
+            editor.model.document.selection
+          );
+        });
+      })
+      .catch((err) => console.error(err));
+  }
 
-	refresh() {
-		const imageCommand = this.editor.commands.get(
-			'imageUpload'
-		);
-		const linkCommand = this.editor.commands.get(
-			'link'
-		);
-		this.isEnabled =
-			true ||
-			( imageCommand &&
-				linkCommand &&
-				( imageCommand.isEnabled ||
-					linkCommand.isEnabled ) );
-	}
+  refresh() {
+    const imageCommand = this.editor.commands.get("imageUpload");
+    const linkCommand = this.editor.commands.get("link");
+    this.isEnabled =
+      true ||
+      (imageCommand &&
+        linkCommand &&
+        (imageCommand.isEnabled || linkCommand.isEnabled));
+  }
 
-	execute() {
-		const input = this
-			.input;
-		input.click();
-	}
+  execute() {
+    const input = this.input;
+    input.click();
+  }
 }
 
 export class BatataEditing extends Plugin {
-	pluginName() {
-		return 'BatataEditing';
-	}
+  pluginName() {
+    return "BatataEditing";
+  }
 
-	init() {
-		const editor = this
-			.editor;
-		editor.commands.add(
-			'batata',
-			new BatataCommand(
-				editor
-			)
-		);
-	}
+  init() {
+    const editor = this.editor;
+    editor.commands.add("batata", new BatataCommand(editor));
+  }
 }
 
 export class Batata extends Plugin {
-	pluginName() {
-		return 'Batata';
-	}
+  pluginName() {
+    return "Batata";
+  }
 
-	requires() {
-		return [
-			BatataCommand,
-			BatataUI
-		];
-	}
+  requires() {
+    return [BatataCommand, BatataUI];
+  }
 }
