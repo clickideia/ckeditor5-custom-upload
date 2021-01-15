7edfe60d303c53b003bf50e213e3947ec7103f28
Rafael Goncalves <rafaelgoncalves@riseup.net>
2021-01-15 17:08:26 -0300

    Mostra icone

--- app.js
+++ app.js
@@ -1,33 +1,31 @@
-import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
-import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
-import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
-import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
-import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
+import ClassicEditor from "@ckeditor/ckeditor5-editor-classic/src/classiceditor";
+import Essentials from "@ckeditor/ckeditor5-essentials/src/essentials";
+import Paragraph from "@ckeditor/ckeditor5-paragraph/src/paragraph";
+import Bold from "@ckeditor/ckeditor5-basic-styles/src/bold";
+import Italic from "@ckeditor/ckeditor5-basic-styles/src/italic";
+import {
+  Batata,
+  BatataUI,
+  BatataCommand,
+  BatataEditing,
+} from "./src/custom-upload";
 
-ClassicEditor.create( document.querySelector( '#editor' ), {
-	plugins: [
-		Essentials,
-		Paragraph,
-		Bold,
-		Italic,
-	],
-	toolbar: [
-		'bold',
-		'italic',
-	],
-} )
-	.then(
-		editor => {
-			console.log(
-				'Editor was initialized',
-				editor
-			);
-		}
-	)
-	.catch(
-		error => {
-			console.error(
-				error.stack
-			);
-		}
-	);
+ClassicEditor.create(document.querySelector("#editor"), {
+  plugins: [
+    Essentials,
+    Paragraph,
+    Bold,
+    Italic,
+    Batata,
+    BatataUI,
+    BatataCommand,
+    BatataEditing,
+  ],
+  toolbar: ["bold", "italic", "insertImage"],
+})
+  .then((editor) => {
+    console.log("Editor was initialized", editor);
+  })
+  .catch((error) => {
+    console.error(error.stack);
+  });
